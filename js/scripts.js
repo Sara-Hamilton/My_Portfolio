cd39b5506dc9dac24eba17ad189eb93d756fafac
Sara <sara@indeterminate.net>
2018-07-11 11:54:14 -0700

    add hamburger menu to navbar for mobile

--- js/scripts.js
+++ js/scripts.js
@@ -11,10 +11,12 @@
 $(function() {
     $('a[href*=\\#]:not([href=\\#])').click(function() {
         var target = $(this.hash);
+        var navbar = document.getElementById("navBar");
+        var navbarHeight = $(navbar).height();
         target = target.length ? target : $('[name=' + this.hash.substr(1) +']');
         if (target.length) {
             $('html,body').animate({
-              scrollTop: target.offset().top - 70
+              scrollTop: target.offset().top - navbarHeight
             }, 2000);
             return false;
         }
